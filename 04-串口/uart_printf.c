0b7250df64852b0177d5b3f56494fab5b4b23474
XY <1023515576@qq.com>
2022-09-11 23:12:32 +0800

    update uart_printf.c to 3.0

--- 04-串口/uart_printf.c
+++ 04-串口/uart_printf.c
@@ -2,7 +2,7 @@
  * @file uart_printf.c
  * @author X. Y.
  * @brief printf() 串口重定向
- * @version 2.1
+ * @version 3.0
  * @date 2022-9-11
  *
  * @copyright Copyright (c) 2022
@@ -14,8 +14,11 @@
 // 串口号配置
 static UART_HandleTypeDef *debug_huart = &huart1;
 
-// 对于 GCC
-#ifdef __GNUC__
+// 判断是哪个编译器。GCC 会定义 __GNUC__，ARMCCv5 会定义 __ARMCC_VERSION，ARMCCv6 会定义 __GNUC__ 和 __ARMCC_VERSION
+#if (defined __GNUC__) && (!defined __ARMCC_VERSION)
+
+// GCC 编译器
+
 #include <errno.h>
 #include <sys/unistd.h>
 
@@ -48,11 +51,15 @@ int _write(int fd, char *pBuffer, int size)
 
     return (status == HAL_OK ? size : 0);
 }
-#endif
 
-// 对于 ARMCC (MDK)
-#ifdef __arm__
+#else
+
+// MDK 的 ARMCC 编译器
+
 #include <stdio.h>
+#include <rt_sys.h>
+#include <rt_misc.h>
+#include <time.h>
 
 /**
  *
@@ -68,22 +75,101 @@ int fputc(int ch, FILE *stream)
     return (status == HAL_OK ? ch : EOF);
 }
 
-// 以下为关闭半主机模式的代码（防止卡死）
-
+// ARMCC 默认启用半主机模式，重定向 printf 后需要关闭，防止卡死
+// ARMCCv6 和 ARMCCv5 关闭半主机模式的代码不一样，需要判断
+#if __ARMCC_VERSION >= 6000000
+// ARMCCv6
+__asm(".global __use_no_semihosting");
+#elif __ARMCC_VERSION >= 5000000
+// ARMCCv5
 #pragma import(__use_no_semihosting)
+#else
+#error Unsupported compiler
+#endif
 
-//标准库需要的支持函数
-struct __FILE
+// 关闭半主机模式后，需要自己实现一些相关系统函数
+
+const char __stdin_name[] = ":tt";
+const char __stdout_name[] = ":tt";
+const char __stderr_name[] = ":tt";
+
+FILEHANDLE _sys_open(const char *name, int openmode)
 {
-    int handle;
-};
-
-FILE __stdout;
-
-//定义_sys_exit()以避免使用半主机模式
-void _sys_exit(int x)
-{
-    (void)x;
+    return 1;
 }
 
+int _sys_close(FILEHANDLE fh)
+{
+    return 0;
+}
+
+char *_sys_command_string(char *cmd, int len)
+{
+    return NULL;
+}
+
+int _sys_write(FILEHANDLE fh, const unsigned char *buf, unsigned len, int mode)
+{
+    return 0;
+}
+
+int _sys_read(FILEHANDLE fh, unsigned char *buf, unsigned len, int mode)
+{
+    return -1;
+}
+
+void _ttywrch(int ch)
+{
+}
+
+int _sys_istty(FILEHANDLE fh)
+{
+    return 0;
+}
+
+int _sys_seek(FILEHANDLE fh, long pos)
+{
+    return -1;
+}
+
+long _sys_flen(FILEHANDLE fh)
+{
+    return -1;
+}
+
+void _sys_exit(int return_code)
+{
+    while (1)
+        ;
+}
+
+clock_t clock(void)
+{
+    clock_t tmp;
+    return tmp;
+}
+
+void _clock_init(void)
+{
+}
+
+time_t time(time_t *timer)
+{
+    time_t tmp;
+    return tmp;
+}
+
+int system(const char *string)
+{
+    return 0;
+}
+
+char *getenv(const char *name)
+{
+    return NULL;
+}
+
+void _getenv_init(void)
+{
+}
 #endif
