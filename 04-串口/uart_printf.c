929de39a670ac67823f5727ee15efdc5b8750845
TITH <1023515576@qq.com>
2022-09-11 17:35:59 +0800

    添加 uart_printf.c

---
+++ 04-串口/uart_printf.c
@@ -0,0 +1,69 @@
+/**
+ * @file uart_printf.c
+ * @author X. Y.
+ * @brief printf() 串口重定向
+ * @version 2.0
+ * @date 2022-9-11
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include "usart.h"
+
+// 串口号配置
+static UART_HandleTypeDef *debug_huart = &huart1;
+
+// 对于 GCC
+#ifdef __GNUC__
+#include <errno.h>
+#include <sys/unistd.h>
+
+/**
+ * @brief Writes data to a file.
+ *
+ * @param fd File descriptor of file into which data is written.
+ * @param pBuffer Data to be written.
+ * @param size Number of bytes.
+ * @return 成功则返回写入的字节数，否则返回 -1
+ */
+int _write(int fd, char *pBuffer, int size)
+{
+	HAL_StatusTypeDef status = HAL_ERROR;
+
+    switch (fd)
+    {
+        case STDOUT_FILENO: // 标准输出流
+            status = HAL_UART_Transmit(debug_huart, (uint8_t *)pBuffer, size, HAL_MAX_DELAY);
+            break;
+        case STDERR_FILENO: // 标准错误流
+            status = HAL_UART_Transmit(debug_huart, (uint8_t *)pBuffer, size, HAL_MAX_DELAY);
+            break;
+        default:
+            // EBADF, which means the file descriptor is invalid or the file isn't opened for writing;
+            errno = EBADF;
+            return -1;
+            break;
+    }
+
+    return (status == HAL_OK ? size : 0);
+}
+#endif
+
+// 对于 ARMCC (MDK)
+#ifdef __arm__
+#include <stdio.h>
+/**
+ * 
+ * @brief 将字符写入流
+ * 
+ * @param ch 要写入的字符
+ * @param stream 流
+ * @return 返回写入的字符，但如果出现错误，返回 EOF
+ */
+int fputc(int ch, FILE *stream)
+{
+    HAL_StatusTypeDef status = HAL_UART_Transmit(debug_huart, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
+    return (status == HAL_OK ? ch : EOF);
+}
+#endif
